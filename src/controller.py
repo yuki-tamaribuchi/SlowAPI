186b899cd2e4e76e4dbd9e0a34a3b3755a279c83
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-11 00:03:44 +0900

    Create UsersController class

--- src/controller.py
+++ src/controller.py
@@ -1,12 +1,12 @@
+from framework.controller.base import ControllerBase
+
+from database import engine
+
 def sample_controller(request_dict):
 	def get(request_dict):
 		return 'Hello from sample controller'
 
-class SampleController:
-	def __init__(self, request_dict):
-		self.request_dict = request_dict
-
-		
+class SampleController(ControllerBase):
 
 	def get(self):
 		return 200, {}, 'Hello from SampleController Get'
@@ -19,3 +19,29 @@ class SampleController:
 
 	def delete(self):
 		return 200, {}, 'Hello from SampleController Delete.'
+
+
+class UsersController(ControllerBase):
+
+	def get(self):
+		import json
+
+
+		with engine.connect() as cnx:
+			result = cnx.execute(
+				"""
+				SELECT * FROM users WHERE username="yuki";
+				"""
+			)
+			user = result.fetchone()
+
+		data = {
+			'id':user[0],
+			'username':user[1],
+			'handle':user[3],
+			#'profile_image':user[4]
+		}
+
+		data = json.dumps(data)
+
+		return 200, {}, data
