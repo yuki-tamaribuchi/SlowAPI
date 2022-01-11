872132bd7ebd18b6648dced117cccae12689ea29
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-12 00:07:25 +0900

    Add path parameter

--- src/urls.py
+++ src/urls.py
@@ -2,5 +2,6 @@ from controller import sample_controller, SampleController, UsersController
 
 url_patterns = [
 	{'url':'sample' ,'controller':SampleController},
-	{'url':'users', 'controller':UsersController}
+	{'url':'users/<username>/', 'controller':UsersController},
+	{'url':'users/<username>/entry/<entry_id>', 'controller':None}
 ]
