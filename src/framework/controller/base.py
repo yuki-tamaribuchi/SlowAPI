36faeda1c90b96d8e86404418b962e05e5bda5ce
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-14 00:04:46 +0900

    Create ControllerAbstractClass

--- src/framework/controller/base.py
+++ src/framework/controller/base.py
@@ -1,3 +1,26 @@
-class ControllerBase:
-	def __init__(self, request_dict):
+from abc import ABCMeta, abstractmethod
+
+
+class ControllerAbstractClass:
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def get(self) -> dict:
+		raise NotImplementedError
+
+	@abstractmethod
+	def post(self) -> dict:
+		raise NotImplementedError
+
+	@abstractmethod
+	def put(self) -> dict:
+		raise NotImplementedError
+
+	@abstractmethod
+	def delete(self) -> dict:
+		raise NotImplementedError
+
+
+class ControllerBase(ControllerAbstractClass):
+	def __init__(self, request_dict:dict) -> None:
 		self.request_dict = request_dict
