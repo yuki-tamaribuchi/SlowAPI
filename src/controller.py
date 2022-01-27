621c9237e3a7245df6f9fe7a12155d658130385d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-28 00:01:20 +0900

    Modify controller

--- src/controller.py
+++ src/controller.py
@@ -27,28 +27,63 @@ class UsersController(ControllerBase):
 	def get(self):
 		import json
 
+		data = ""
+
 		username = self.request_dict['path_parameters']['username']
 
+		if not username	== "":
 
-		with engine.connect() as cnx:
-			result = cnx.execute(
-				"""
-				SELECT * FROM users WHERE username="{}";
-				""".format(username)
-			)
-			user = result.fetchone()
+			with engine.connect() as cnx:
+				result = cnx.execute(
+					"""
+					SELECT * FROM users WHERE username="{}";
+					""".format(username)
+				)
+				user = result.fetchone()
+		
+			if user is not None:
 
-		data = {
-			'id':user[0],
-			'username':user[1],
-			'handle':user[3],
-			#'profile_image':user[4]
-		}
+				data = {
+					'id':user[0],
+					'username':user[1],
+					'handle':user[3],
+					#'profile_image':user[4]
+				}
 
-		data = json.dumps(data)
+				data = json.dumps(data)
+				status_code = 200
+
+			else:
+				status_code = 404
+		else:
+			with engine.connect() as cnx:
+				result = cnx.execute(
+					"""
+					SELECT id, username
+					FROM users;
+					"""
+				)
+				users = result.fetchall()
+			
+			users_arr = []
+			for user in users:
+				user_dict = {
+					'id':user[0],
+					'username':user[1]
+				}
+				users_arr.append(user_dict)
+	
+			
+			data = {
+				'users':users_arr
+			}
+			data = json.dumps(data)
+			status_code = 200
+
+
 
 		controller_response_dict = generate_controller_response_dict(
-			status_code=200,
+			status_code=status_code,
 			custom_headers={},
 			body=data
 		)
