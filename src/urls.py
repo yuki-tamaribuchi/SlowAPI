1e62bc3937da53321ff41d7a08390412a702793d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-28 00:02:07 +0900

    Modify url pattern

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,15 @@
 from controller import sample_controller, SampleController, UsersController
 
+from framework.middleware.security import AllowedHostMiddleware, SQLInjectionProtectMiddleware
+from framework.middleware.sample import PrintRequestMiddleware, PrintResponseMiddleware
+
+#{'url':'任意のURL', 'controller':任意のControllクラス, 'middlewares':[任意のミドルウェアクラス<0個以上>]}
+#パスパラメータは<>で括る
+#{'url':, 'controller':, 'middlewares':[]} <-コピペ用
+
 url_patterns = [
-	{'url':'sample' ,'controller':SampleController},
-	{'url':'users/<username>/', 'controller':UsersController},
-	{'url':'users/<username>/entry/<entry_id>', 'controller':None}
+	{'url':'sample' ,'controller':SampleController, 'middlewares':[]},
+	{'url':'users/<username>', 'controller':UsersController, 'middlewares':[SQLInjectionProtectMiddleware, PrintRequestMiddleware, PrintResponseMiddleware]},
+	{'url':'users/<username>/entry/<entry_id>', 'controller':None, 'middlewares':[]},
+	{'url':'sample/sample/<username>', 'controller':SampleController, 'middlewares':[]}
 ]
