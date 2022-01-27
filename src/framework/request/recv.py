a634f730c9ded8a49c728fcb306a015e8d24b958
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-28 00:05:49 +0900

    Add code doesn't work now

--- src/framework/request/recv.py
+++ src/framework/request/recv.py
@@ -1,2 +1,34 @@
 def recv_request(client_sock):
 	return client_sock.recv(4096)
+
+
+
+"""
+受信するデータ量を拡張するため、複数回recvを呼び出そうとしたが、受信データが終わった時点で、ソケットがハングする。
+setblockingをFalseに設定やタイムアウトの設定をしたがBlockingIOError: [Errno 11] Resource temporarily unavailableがでる。
+現状では4096bytes以下ということで開発を行う。
+"""
+
+#import errno
+#
+#def recv_all(client_sock):
+#	#client_sock.setblocking(False)
+#
+#	try:
+#		chunk = client_sock.recv(16)
+#		return chunk
+#	except IOError as e:
+#		if e.errno == errno.EWOULDBLOCK:
+#			print('err called')
+#			recv_all(client_sock)
+#
+#def recv_request(client_sock):
+#	#client_sock.setblocking(False)
+#	request = b''
+#
+#	while True:
+#		chunk = recv_all(client_sock)
+#		if not chunk: break
+#		request+=chunk
+#	print(request)
+#	return request
