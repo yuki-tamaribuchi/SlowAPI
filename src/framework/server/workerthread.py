d71e85d6b56d9dbad3658bee9972c68bba77b7cf
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-28 00:06:43 +0900

    Moidfy WorkerThread class

--- src/framework/server/workerthread.py
+++ src/framework/server/workerthread.py
@@ -3,40 +3,63 @@ import datetime
 
 from framework.request.base import load_request
 from framework.request.recv import recv_request
-from framework.response.generate import generate_response_body_from_file
+from framework.response.generate import generate_response_dict
 from framework.response.send import send_response
 from framework.http.get import get
-from framework.dispatcher.dispatcher import dispatcher
+from framework.dispatcher.dispatcher import dispatch
+from framework.urls.resolver import resolve
+from framework.middleware.loader import load as load_middlewares
+from framework.middleware.executer import execute_request_middlewares, execute_response_middlewares
+from framework.exception.security import HostNotAllowedException
+
+from urls import url_patterns
+from settings.middleware import MIDDLEWARES
 
 
 class WorkerThread(Thread):
 	def __init__(self, client_sock, client_addr):
 		self.client_sock = client_sock
 		self.client_addr = client_addr
+		self.request_dict = {}
+		self.response_dict = {}
 
 
-	def request(self):
+	def recv_and_load_request(self):
 		request = recv_request(self.client_sock)
 		self.request_dict = load_request(request)
 		print('{} {} {} {}'.format(datetime.datetime.now().strftime("%c"), self.client_addr[0], self.request_dict['line']['method'], self.request_dict['line']['uri']))
 
-	
-	def dispatch(self):
-		self.response_dict=dispatcher(self.request_dict)
+	def url_resolve(self):
+		self.request_dict = resolve(self.request_dict, url_patterns)
 
+	def load_middleware(self):
+		self.request_dict = load_middlewares(self.request_dict, MIDDLEWARES)
+
+	def exc_request_middleware(self):
+		try:
+			self.request_dict = execute_request_middlewares(self.request_dict)
+		except HostNotAllowedException as e:
+			self.response_dict = generate_response_dict(400, "", "Host not allowed")
+			self.response()
+			raise e
+
+	def dispatch_request(self):
+		self.response_dict=dispatch(self.request_dict)
+
+	def exc_response_middleware(self):
+		self.response_dict = execute_response_middlewares(self.request_dict, self.response_dict)
 
 	def response(self):
 		send_response(self.client_sock, self.response_dict)
-		#if self.request_dict['line']['method']=="GET":
-		#	get(self.client_sock, self.request_dict['line']['uri'])
-		#	
-		#else:
-		#	send_response(self.client_sock, 501)
 
 
 	def call(self):
-		self.request()
-		self.dispatch()
+		self.recv_and_load_request()
+		self.url_resolve()
+		self.load_middleware()
+		self.exc_request_middleware()
+		self.dispatch_request()
+		self.exc_response_middleware()
 		self.response()
 
 
