f1dabaf586380fb7f94a22f3eba97733df8778b5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-28 00:03:08 +0900

    Modify SQLInjectionProtectMiddleware class

--- src/framework/middleware/security.py
+++ src/framework/middleware/security.py
@@ -3,6 +3,7 @@ import re
 
 from framework.middleware.base import RequestMiddlewareBase
 from framework.exception.security import HostNotAllowedException
+from framework.utils.security import remove_special_characters, escape_single_and_double_quote_to_html_entity
 
 from settings.hosts import HOSTS
 
@@ -23,16 +24,33 @@ class AllowedHostMiddleware(RequestMiddlewareBase):
 		raise HostNotAllowedException(host)
 
 
+
 class SQLInjectionProtectMiddleware(RequestMiddlewareBase):
 
+	exclude_from_special = "&=;"
+	special_chars = re.escape(re.sub("["+exclude_from_special+"]", "", string.punctuation))
+
 	def __call__(self) -> dict:
-		special_chars = re.escape(string.punctuation).replace("/", "")
+
 		sub_path_parameters = {}
 		if self.request_dict['path_parameters']:
 			for k, v in self.request_dict['path_parameters'].items():
-				sub_v = re.sub(r'['+special_chars+r']', "", v)
-				sub_path_parameters[k] = sub_v
+				escaped_v = escape_single_and_double_quote_to_html_entity(v)
+				sub_escaped_v = remove_special_characters(escaped_v, self.special_chars)
+				sub_path_parameters[k] = sub_escaped_v
 			self.request_dict['path_parameters'] = sub_path_parameters
+
+		if self.request_dict['line']['query']:
+			escaped_query = escape_single_and_double_quote_to_html_entity(self.request_dict['line']['query'])
+			sub_escaped_query = remove_special_characters(escaped_query, self.special_chars)
+			self.request_dict['line']['query'] = sub_escaped_query
+		
+		if self.request_dict['body']:
+			escaped_body = escape_single_and_double_quote_to_html_entity(self.request_dict['body'])
+			sub_escaped_body = remove_special_characters(escaped_body, self.special_chars)
+			self.request_dict['body'] = sub_escaped_body
+	
+			
 		return self.request_dict
 
 
