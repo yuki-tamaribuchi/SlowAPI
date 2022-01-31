889783e1b4df8968d8a2170dd987d38864a02b21
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-01 00:03:48 +0900

    Modify server

--- src/framework/server/server.py
+++ src/framework/server/server.py
@@ -10,25 +10,21 @@ class Server:
 	def serve(self):
 		
 
-		try:
-			print('Server was started at {}:{}'.format(self.ip_addr, self.port))
-			sock = socket.socket(
-				family=socket.AF_INET,
-				type=socket.SOCK_STREAM
-			)
-			sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		print('Server was started at {}:{}'.format(self.ip_addr, self.port))
+		sock = socket.socket(
+			family=socket.AF_INET,
+			type=socket.SOCK_STREAM
+		)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-			sock.bind((self.ip_addr, self.port))
+		sock.bind((self.ip_addr, self.port))
 
-			sock.listen(5)
+		sock.listen(5)
 
-			while True:
-				client_sock, client_addr = sock.accept()
+		while True:
+			client_sock, client_addr = sock.accept()
 
-				thread = WorkerThread(client_sock, client_addr)
-				thread.start()
+			thread = WorkerThread(client_sock, client_addr)
+			thread.start()
 
-				client_sock.close()
-		finally:
-			sock.close()
-			print('Server was stopped')
+			client_sock.close()
