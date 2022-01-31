8c8028fa801021049ce39190fa80a3245866c35b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-01 00:03:23 +0900

    Modify sample middleware

--- src/framework/middleware/sample.py
+++ src/framework/middleware/sample.py
@@ -1,7 +1,16 @@
-from framework.middleware.base import ResponseMiddlewareBase
+from framework.middleware.base import ResponseMiddlewareBase, RequestMiddlewareBase
 
-class SampleResponseMiddleware(ResponseMiddlewareBase):
+
+class PrintRequestMiddleware(RequestMiddlewareBase):
+
+	def __call__(self) -> dict:
+		print("Request:")
+		print(self.request_dict)
+		return self.request_dict
+
+class PrintResponseMiddleware(ResponseMiddlewareBase):
 	
 	def __call__(self):
+		print("Response")
 		print(self.response_dict)
 		return self.response_dict
