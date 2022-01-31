a64c716db6941020e13f2702ff32a6d5bf22859e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-01 00:00:48 +0900

    Add logger

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+import logging
+import logging.config
+from os import path
+
 from framework.server.server import Server
 from settings import *
 from settings.server import SERVER_LISTEN_IP_ADDR, SERVER_LISTEN_PORT
 
+logging_config_filepath = path.join(path.dirname(path.abspath(__file__)), "logging.conf")
+logging.config.fileConfig(logging_config_filepath, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
 try:
 	server = Server(SERVER_LISTEN_IP_ADDR, SERVER_LISTEN_PORT)
 	server.serve()
+except Exception as e:
+	logger.exception(e)
 except KeyboardInterrupt:
 	pass
