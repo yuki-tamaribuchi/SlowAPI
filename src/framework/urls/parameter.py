f550f9dc5fd0eae711ff8a89b9d9a1e5818fb17d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-01 00:04:53 +0900

    Add comments

--- src/framework/urls/parameter.py
+++ src/framework/urls/parameter.py
@@ -10,38 +10,67 @@ def create_path_parameters_mask(url_patterns):
 
 	return path_parameters_masks
 
+#パスパラメータのname配列とvalue配列を作成
+def extract_path_parameter(url, url_pattern, path_parameters_masks):
 
-def extract_path_parameter(url, url_pattern, path_parameters_mask):
-	if len(path_parameters_mask)==1 and path_parameters_mask[0]==False:
+	#パスパラメータマスク配列の要素数が1、かつ、その要素がFalseの場合
+	#つまり、パスパラメータを使用していない場合
+	if len(path_parameters_masks)==1 and path_parameters_masks[0]==False:
 		return None, None
 
+	#url_patternを/でスプリット
 	splitted_url_pattern = url_pattern['url'].split("/")
 
+	#空の文字列がリストに含まれている場合削除
 	if "" in splitted_url_pattern:
 		splitted_url_pattern.remove("")
 
+	#urlのスプリット
 	splitted_url = url.split("/")
 
+	#上記と同じ
 	if "" in splitted_url:
 		splitted_url.remove("")
+
+	#パスパラメータが渡されていない場合、空文字を追加
+	if len(splitted_url_pattern)-1==len(splitted_url):
+		splitted_url.append("")
+	#パスパラメータが渡されている場合はそのまま
+	elif len(splitted_url_pattern)==len(splitted_url):
+		pass
+	#それ以外はエラー
+	else:
+		print("Path parameter name and value error")
 	
-	path_parameters_name = [token for mask, token in zip(path_parameters_mask, splitted_url_pattern) if mask]
-	path_parameters_value = [token for mask, token in zip(path_parameters_mask, splitted_url) if mask]
-
-	return path_parameters_name, path_parameters_value
 
 
+	#マスクがTrueのところだけ取り出し
+	path_parameters_name_arr = [token for mask, token in zip(path_parameters_masks, splitted_url_pattern) if mask]
+	path_parameters_value_arr = [token for mask, token in zip(path_parameters_masks, splitted_url) if mask]
 
-def create_path_parameter_dict(path_parameters_name, path_parameters_value):
-	if not (len(path_parameters_name)==len(path_parameters_value)):
+
+	return path_parameters_name_arr, path_parameters_value_arr
+
+
+#パスパラメータのディクショナリを作成する
+#パスパラメータのnameとvalueを受けとって、ディクショナリにして返す
+def create_path_parameter_dict(path_parameters_name_arr, path_parameters_value_arr):
+
+	#配列の要素数が同じでなければエラー
+	if not (len(path_parameters_name_arr)==len(path_parameters_value_arr)):
 		print('Path parameter error')
 		return
 
 	path_parameters_dict = {}
 
-	for i in range(len(path_parameters_name)):
-		if path_parameters_value[i]:
-			name=path_parameters_name[i][1:-1]
-			path_parameters_dict[name]=path_parameters_value[i]
+	for i in range(len(path_parameters_name_arr)):
+
+		#パラメータのバリューがNoneでなければ回す
+		if path_parameters_value_arr[i] is not None:
+
+			#<>を取り除く
+			name=path_parameters_name_arr[i][1:-1]
+
+			path_parameters_dict[name]=path_parameters_value_arr[i]
 
 	return path_parameters_dict
