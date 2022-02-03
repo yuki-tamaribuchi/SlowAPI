5c7261350b053861b3177e37117c72782caad5ac
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-04 00:08:12 +0900

    Add http status error code 422

--- slowapi/response/generate.py
+++ slowapi/response/generate.py
@@ -4,6 +4,7 @@ def generate_http_response_line(status_code):
 			400:'400 Bad Request',
 			404:'404 Not Found',
 			405:'405 Method Not Allowed',
+			422:'422 Unprocessable Entity',
 			500:'500 Internal Server Error',
 			501:'501 Not Implemented'
 		}
